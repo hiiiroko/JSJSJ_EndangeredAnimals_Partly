795c6f4a2203e2d242b4f33a05a39324d7c6cd7f
cheems <hiiiroko@proton.me>
2023-04-18 21:07:45 +0800

    build(template): Created logical pages.

---
+++ src/components/distribution/Distribution.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Distribution.css'
+
+function Distribution() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Distribution">
+      
+    </div>
+  )
+}
+
+export default Distribution
---
+++ src/components/ui/UI.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './UI.css'
+
+function UI() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="UI">
+      
+    </div>
+  )
+}
+
+export default UI
