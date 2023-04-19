2365dc50e29ed52c7ec3da3f4215fbb288686d01
cheems <hiiiroko@proton.me>
2023-04-19 16:49:30 +0800

    feat(ui): Added simple ui.

--- src/components/protection/Protection.jsx
+++ src/components/protection/Protection.jsx
@@ -3,12 +3,27 @@ import './Protection.css'
 import './P_Style.css'
 
 function Protection() {
-  const imgUrl = [
-    { backgroundImage: `url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')` },
-    { backgroundImage: `url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')` },
-    { backgroundImage: `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')` },
-    { backgroundImage: `url('https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')` },
-    { backgroundImage: `url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')` }
+  const items = [
+    {
+      backgroundImage: `url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+      slogan: `Explore The World`
+    },
+    {
+      backgroundImage: `url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+      slogan: `Wild Forest`
+    },
+    {
+      backgroundImage: `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')`,
+      slogan: `Sunny Beach`
+    },
+    {
+      backgroundImage: `url('https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')`,
+      slogan: `City on Winter`
+    },
+    {
+      backgroundImage: `url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+      slogan: `Mountains - Clouds`
+    }
   ]
 
   const panels = document.querySelectorAll('.panel')
@@ -29,24 +44,16 @@ function Protection() {
   return (
     <div className="Protection">
       <div class="container">
-        <div class="panel active" style={imgUrl[0]}>
-          <h3>Explore The World</h3>
-        </div>
-        <div class="panel" style={imgUrl[1]}>
-          <h3>Wild Forest</h3>
-        </div>
-        <div class="panel" style={imgUrl[2]}>
-          <h3>Sunny Beach</h3>
-        </div>
-        <div class="panel" style={imgUrl[3]}>
-          <h3>City on Winter</h3>
-        </div>
-        <div class="panel" style={imgUrl[4]}>
-          <h3>Mountains - Clouds</h3>
-        </div>
+        {items.map((item, i) => {
+          return (
+            <div class={`panel${i == 0 ? ' active' : ''}`} style={item} >
+              <h3>{item.slogan}</h3>
+            </div>
+          )
+        })}
       </div>
 
-    </div>
+    </div >
   )
 }
 
--- src/components/ui/UI.jsx
+++ src/components/ui/UI.jsx
@@ -2,17 +2,31 @@ import { useState } from 'react'
 import './UI.css'
 import '../protection/Protection'
 import '../distribution/Distribution'
+import Distribution from '../distribution/Distribution'
+import Protection from '../protection/Protection'
 
 function UI() {
-  const [count, setCount] = useState(0)
+  const [scene, setScene] = useState('distribution')
+  const switchScene = (arg) => {
+    setScene(arg)
+    console.log(`Switch to ${arg}.`)
+  }
 
   return (
     <div className="UI">
       <div className='display'>
-        <div className='logo'></div>
-        <div className='content'></div>
+        <div className='logo'>濒危动物调查局</div>
+        <div className='content'>
+          {scene == 'distribution' ? 'Distribution' : ''}
+          {scene == 'protection' ? <Protection /> : ''}
+          {/* {scene == 'protection' ? 'Protection' : ''} */}
+          {/* {scene == 'distribution' ? <Distribution /> : ''} */}
+        </div>
       </div>
-      <div className='control'><button></button></div>
+      <div className='control'>
+        <button onClick={() => switchScene('distribution')}>地区分布</button>
+        <button onClick={() => switchScene('protection')}>保护措施</button>
+      </div>
     </div>
   )
 }
