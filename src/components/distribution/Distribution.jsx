439899c6c44d1a99e9a3f92970dfe48657017075
cheems <hiiiroko@proton.me>
2023-04-20 09:32:16 +0800

    fix(map): Changed minimal realization.

--- src/components/distribution/Distribution.jsx
+++ src/components/distribution/Distribution.jsx
@@ -1,19 +1,65 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './Distribution.css'
+import * as echarts from 'echarts';
+import ReactEcharts from 'echarts-for-react'
 import { Map, Marker, NavigationControl, InfoWindow } from 'react-bmap'
 
 function Distribution() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="Distribution">
+      <div id="container"></div>
+      {/* 使用react-bmap的元素是可以显示地图的 */}
       <Map center={{ lng: 116.402544, lat: 39.928216 }} zoom="11">
         <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
         <NavigationControl />
-        <InfoWindow position={{ lng: 116.402544, lat: 39.928216 }} text="内容" title="标题" />
       </Map>
     </div>
   )
 }
 
 export default Distribution
+
+// import './Distribution.css'
+// import React, { PureComponent } from "react";
+// import * as eCharts from "echarts";
+
+// // 好久没接触类组件了(3MZ 我给你举盾
+// export default class Distribution extends PureComponent {
+
+//   eChartsRef = React.createRef();
+
+//   componentDidMount() {
+//     const myChart = eCharts.init(this.eChartsRef.current);
+
+//     let option = {
+//       title: {
+//         text: "ECharts 入门示例",
+//       },
+//       tooltip: {},
+//       legend: {
+//         data: ["销量"],
+//       },
+//       xAxis: {
+//         data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+//       },
+//       yAxis: {},
+//       series: [
+//         {
+//           name: "销量",
+//           type: "bar",
+//           data: [5, 20, 36, 10, 10, 20],
+//         },
+//       ],
+//     };
+
+//     myChart.setOption(option);
+//   }
+
+//   render() {
+//     return <div ref={this.eChartsRef} style={{
+//       width: 600,
+//       height: 400,
+//     }}></div>;
+//   }
+// }
--- src/components/protection/Protection.jsx
+++ src/components/protection/Protection.jsx
@@ -1,60 +1,104 @@
-import { useState } from 'react'
 import './Protection.css'
-import './P_Style.css'
+import React, { PureComponent } from "react";
+import * as eCharts from "echarts";
 
-function Protection() {
-  const items = [
-    {
-      backgroundImage: `url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
-      slogan: `Explore The World`
-    },
-    {
-      backgroundImage: `url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
-      slogan: `Wild Forest`
-    },
-    {
-      backgroundImage: `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')`,
-      slogan: `Sunny Beach`
-    },
-    {
-      backgroundImage: `url('https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')`,
-      slogan: `City on Winter`
-    },
-    {
-      backgroundImage: `url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
-      slogan: `Mountains - Clouds`
-    }
-  ]
+// 好久没接触类组件了(3MZ 我给你举盾
+export default class Protection extends PureComponent {
 
-  const panels = document.querySelectorAll('.panel')
+  eChartsRef = React.createRef();
 
-  panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-      removeActiveClasses()
-      panel.classList.add('active')
-    })
-  })
+  componentDidMount() {
+    const myChart = eCharts.init(this.eChartsRef.current);
 
-  function removeActiveClasses() {
-    panels.forEach(panel => {
-      panel.classList.remove('active')
-    })
+    let option = {
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+
+    myChart.setOption(option);
   }
 
-  return (
-    <div className="Protection">
-      <div class="container">
-        {items.map((item, i) => {
-          return (
-            <div class={`panel${i == 0 ? ' active' : ''}`} style={item} >
-              <h3>{item.slogan}</h3>
-            </div>
-          )
-        })}
-      </div>
-
-    </div >
-  )
+  render() {
+    return <div ref={this.eChartsRef} style={{
+      width: 600,
+      height: 400,
+    }}></div>;
+  }
 }
 
-export default Protection
+// import { useState } from 'react'
+// import './Protection.css'
+// import './P_Style.css'
+
+// function Protection() {
+//   const items = [
+//     {
+//       backgroundImage: `url('https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+//       slogan: `Explore The World`
+//     },
+//     {
+//       backgroundImage: `url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+//       slogan: `Wild Forest`
+//     },
+//     {
+//       backgroundImage: `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')`,
+//       slogan: `Sunny Beach`
+//     },
+//     {
+//       backgroundImage: `url('https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80')`,
+//       slogan: `City on Winter`
+//     },
+//     {
+//       backgroundImage: `url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+//       slogan: `Mountains - Clouds`
+//     }
+//   ]
+
+//   const panels = document.querySelectorAll('.panel')
+
+//   panels.forEach(panel => {
+//     panel.addEventListener('click', () => {
+//       removeActiveClasses()
+//       panel.classList.add('active')
+//     })
+//   })
+
+//   function removeActiveClasses() {
+//     panels.forEach(panel => {
+//       panel.classList.remove('active')
+//     })
+//   }
+
+//   return (
+//     <div className="Protection">
+//       <div class="container">
+//         {items.map((item, i) => {
+//           return (
+//             <div class={`panel${i == 0 ? ' active' : ''}`} style={item} >
+//               <h3>{item.slogan}</h3>
+//             </div>
+//           )
+//         })}
+//       </div>
+
+//     </div >
+//   )
+// }
+
+// export default Protection
